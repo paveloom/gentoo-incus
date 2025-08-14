a1033fb446acf501b4ed08e9a53508e4f840009d
Pavel Sobolev <contact@paveloom.dev>
2025-08-14 16:04:17 +0300

    Bash: disable XON/XOFF flow control.

--- configuration/etc/bash/bashrc.d/90-custom-history.bash
+++ configuration/etc/bash/bashrc.d/90-custom-history.bash
@@ -3,3 +3,7 @@
 export HISTCONTROL=erasedups
 export HISTFILESIZE=10000
 export HISTSIZE=1000
+
+# Disable XON/XOFF flow control, thus allowing the
+# use of `C-s` for forward searching the history
+stty -ixon
