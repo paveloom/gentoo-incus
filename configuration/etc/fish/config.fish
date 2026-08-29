1748bfe65df37996b5e0c7cf06b4da91ee840993
Pavel Sobolev <contact@paveloom.dev>
2026-08-29 16:16:18 +0300

    Fish: make `Ctrl+Left` and `Ctrl+Right` move between words

--- configuration/etc/fish/config.fish
+++ configuration/etc/fish/config.fish
@@ -7,3 +7,7 @@ set -x LESSCOLORIZER "pygmentize -f 256"
 
 # Bind `Ctrl+Backspace` to delete a word behind the cursor
 bind ctrl-backspace backward-kill-word
+
+# Make `Ctrl+Left` and `Ctrl+Right` move between words
+bind ctrl-left prevd-or-backward-word
+bind ctrl-right nextd-or-forward-word
